05dedf2e7b3cd452243a2c7f505d775020d21edc
Aenne Brielmann <aenne.brielmann@nyu.edu>
2023-02-22 14:07:23 +0100

    style improvements

--- analysis/f_merge_fit_results_participantInfo_cv.py
+++ analysis/f_merge_fit_results_participantInfo_cv.py
@@ -11,18 +11,18 @@ import os
 import glob
 import pandas as pd
 
-#%% ---------------------------------------------------------
+# %% ---------------------------------------------------------
 # Specify directories
-#------------------------------------------------------------
+# ------------------------------------------------------------
 os.chdir('..')
 home_dir = os.getcwd()
 dataDir = home_dir
 # list the names of all models as in the saved .csv files
 allResFiles = glob.glob(dataDir + '/results/*.csv')
 
-#%% ---------------------------------------------------------
+# %% ---------------------------------------------------------
 # Load, merge, save the data
-#------------------------------------------------------------
+# ------------------------------------------------------------
 infoDf = pd.read_csv(dataDir + '/merged_participantInfo.csv')
 df = infoDf.copy()
 
@@ -31,13 +31,13 @@ for file in allResFiles:
     modelSpec = file.replace(dataDir+'/results', '')
     # windows/OS encoding differs a little in use of \\ vs /
     # remove both
-    modelSpec = modelSpec.replace('\\','')
-    modelSpec = modelSpec.replace('/','')
-    modelSpec = modelSpec.replace('.csv','')
-    modelSpec = modelSpec.replace('cv','')
-    modelSpec = modelSpec.replace('_results','')
-    modelSpec = modelSpec.replace('_paper_draftmodel','')
-    resDf = modelDf.iloc[:,-5:-1].add_suffix(modelSpec)
+    modelSpec = modelSpec.replace('\\', '')
+    modelSpec = modelSpec.replace('/', '')
+    modelSpec = modelSpec.replace('.csv', '')
+    modelSpec = modelSpec.replace('cv', '')
+    modelSpec = modelSpec.replace('_results', '')
+    modelSpec = modelSpec.replace('_paper_draftmodel', '')
+    resDf = modelDf.iloc[:, -5:-1].add_suffix(modelSpec)
     resDf['subj'] = modelDf['subj'].copy()
     df = pd.merge(df, resDf, on='subj')
 
